01c772297c7182706cd68b24fb2f34e8420300a6
eliss <elis.staaf@proton.me>
2024-12-28 14:43:59 +0100

    Add -r

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ SILK_BASE_DEST=/usr/include/silk.h
 SILK_EXTE_SRC=src/silk
 SILK_EXTE_DEST=/usr/include/silk.h
 
-sudo cp \
+sudo cp -r \
     $SILK_BASE_SRC \
     $SILK_BASE_DEST
-sudo cp \
+sudo cp -r \
     $SILK_EXTE_SRC \
     $SILK_EXTE_DEST
