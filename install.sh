20d01bb1d72eae2ba2b62caa720492cd6d59aff9
eliss <elis.staaf@proton.me>
2024-12-28 14:44:59 +0100

    dbg

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 SILK_BASE_SRC=src/silk.h
 SILK_BASE_DEST=/usr/include/silk.h
 SILK_EXTE_SRC=src/silk
-SILK_EXTE_DEST=/usr/include/silk.h
+SILK_EXTE_DEST=/usr/include/silk
 
 sudo cp -r \
     $SILK_BASE_SRC \
